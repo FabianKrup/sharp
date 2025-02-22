73adf33ac9534ea6a49993cb7e14b257d900f4d7
Fabian Krupinski <fabian.krupinski@next-levels.de>
2025-02-22 06:34:54 +0100

    feat: :sparkles: Working sharp conversion

--- index.js
+++ index.js
@@ -1,10 +1,8 @@
 const sharp = require("sharp");
-const { exec } = require("child_process");
 const fs = require("fs");
 const path = require("path");
 
 const inputPath = path.resolve(process.cwd(), "image.heif");
-const outputPath = path.resolve(process.cwd(), "output.png");
 
 fs.stat(inputPath, (err, stats) => {
   if (err) {
@@ -22,38 +20,8 @@ async function main() {
   console.log("LD_LIBRARY_PATH:", process.env.LD_LIBRARY_PATH);
   console.log("Current working directory:", process.cwd());
 
-  exec("which vips", (error, stdout, stderr) => {
-    if (error) {
-      console.error("vips not found in PATH");
-    } else {
-      console.log("vips location:", stdout);
-    }
-  });
-
-  exec(
-    `vips copy "${inputPath}" "${outputPath}"`,
-    {
-      env: {
-        ...process.env,
-        PATH: "/usr/local/bin:" + process.env.PATH,
-        LD_LIBRARY_PATH: "/usr/local/lib:" + process.env.LD_LIBRARY_PATH,
-      },
-    },
-    (error, stdout, stderr) => {
-      if (error) {
-        console.error(`Error: ${error.message}`);
-        return;
-      }
-      if (stderr) {
-        console.error(`Stderr: ${stderr}`);
-        return;
-      }
-      console.log(`Stdout: ${stdout}`);
-    }
-  );
-
   try {
-    await image.toFormat("png").toFile("output-sharp.png");
+    await image.toFormat("png").toFile("/output/output-sharp.png");
     console.log("Conversion successful using sharp");
   } catch (err) {
     console.error("Sharp conversion error:", err);
