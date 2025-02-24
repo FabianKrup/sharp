29c6367bbed6a9f49030cbbaac190e28fd33981b
Fabian Krupinski <fabian.krupinski@next-levels.de>
2025-02-24 20:42:29 +0100

    refactor: :recycle:

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const fs = require("fs");
 const sharp = require("sharp");
 
+console.log("sharp versions", sharp.versions);
+
 async function main() {
   try {
     const inputFileBuffer = fs.readFileSync("image.heif");
